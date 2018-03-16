55105a6392afdf9403b3b8140ba2f0258d4f5d27
Anand Singh <sanand926@gmail.com>
2018-03-16 12:04:10 +0530

    imageThumbnail bug fix

--- src/Book.js
+++ src/Book.js
@@ -15,18 +15,19 @@ class Book extends Component{
             <li>
                 <div className="book">
                     <div className="book-top">
-                        <div className="book-cover" style={{backgroundImage: `url(${book.imageLinks.thumbnail})`, width: 128, height: 174 }}></div>
+                        <div className="book-cover" style={{backgroundImage: `url(${book.imageLinks !== undefined ? book.imageLinks.thumbnail:''})`, width: 128, height: 174 }}></div>
                         <div className="book-shelf-changer">
                             <select value={book.shelf} onChange={(event) => this.props.onChangeShelf(book,event.target.value)}>
                                 <option value="none" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
                                 <option value="read">Read</option>
+                                <option value="none">None</option>
                             </select>
                         </div>
                     </div>
                     <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors[0]}</div>
+                    <div className="book-authors">{book.authors}</div>
                 </div>
             </li>
         )
